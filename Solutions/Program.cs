66b5bb4a9149d040ab0da6defe0deb0f5be73810
Lee Beenen <l.beenen@diract.nl>
2016-12-03 18:14:41 +0100

    Initial commit

---
+++ Solutions/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Solutions
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      //Days.Day1();
+      //Days.Day2();
+      Days.Day3();
+      Console.ReadLine();
+    }
+  }
+}
