0c2e2f568da62566589015aefcdbccf9b5257d9e
Lee Beenen <l.beenen@diract.nl>
2016-12-08 17:17:25 +0100

    Solved day7

--- Solutions/Models/Day7/PalindromeHelper.cs
+++ Solutions/Models/Day7/PalindromeHelper.cs
@@ -2,34 +2,34 @@ using System.Collections.Generic;
 
 namespace Solutions.Models.Day7
 {
-    public static class PalindromeHelper
+  public static class PalindromeHelper
+  {
+    public static bool ContainsFourLetterPalindrome(string input)
     {
-        public static bool ContainsFourLetterPalindrome(string input)
+        for(var idx = 0; idx < input.Length - 3; idx++)
         {
-            for(var idx = 0; idx < input.Length - 3; idx++)
+            if(input[idx] == input[idx + 3] && input[idx + 1] == input[idx + 2] && input[idx] != input[idx + 1])
             {
-                if(input[idx] == input[idx + 3] && input[idx + 1] == input[idx + 2] && input[idx] != input[idx + 1])
-                {
-                    return true;
-                }
+                return true;
             }
-
-            return false;
         }
 
-        public static string[] ABACheck(string input)
-        {
-            var abaCollection = new List<string>();
-
-            for(var idx = 0; idx < input.Length - 2; idx++)
-            {
-                if(input[idx] == input[idx + 2] && input[idx] != input[idx + 1])
-                {
-                    abaCollection.Add(string.Join("", new []{ input[idx], input[idx + 1], input[idx + 2] }));
-                }
-            }
-
-            return abaCollection.ToArray();
-        }
+        return false;
     }
+
+    public static string[] ABACheck(string input)
+    {
+        var abaCollection = new List<string>();
+
+        for(var idx = 0; idx < input.Length - 2; idx++)
+        {
+            if(input[idx] == input[idx + 2] && input[idx] != input[idx + 1])
+            {
+                abaCollection.Add(string.Join("", new []{ input[idx], input[idx + 1], input[idx + 2] }));
+            }
+        }
+
+        return abaCollection.ToArray();
+    }
+  }
 }
